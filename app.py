1318714860bf646f624fe2b133d26398e0f722e2
Pablo Souza Grigoletti <pablo.grigoletti@serpro.gov.br>
2024-08-20 23:02:08 -0300

    Primeiro app

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.title('Consultar curvas de crescimento')
+
+doenca = st.selectbox("Doença", ["Sem doença"])
+sexo = st.selectbox("Sexo", ["Masculino", "Feminino"])
+idade_em_anos = st.number_input("Idade em anos", step=1)
+peso_em_kg = st.number_input("Peso em Kg")
+altura_em_cm = st.number_input("Altura em Cm")
+if st.button("Consultar"):
+    st.write("Gerar gráfico...")
